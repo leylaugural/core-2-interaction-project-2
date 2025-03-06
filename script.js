85e7362175fc9802e6faeaaa8c187ddcb7e54dc3
leylaugural <ugurl672@newschool.edu>
2025-03-06 03:58:45 -0500

    a lot

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
 function generateFlavor() {
-    const imgElement = document.getElementById("displayedImage");
-    const flavorOutput = document.getElementById("flavorOutput");
+    let flavorOutput = document.getElementById("flavorOutput");
+    let body = document.body;
 
 
-    const input1_letter = document.getElementById("charInput1").value.toUpperCase();
-    const input2_letter = document.getElementById("charInput2").value.toUpperCase();
+    let flavor1 = document.getElementById("flavorSelect1").value;
+    let flavor2 = document.getElementById("flavorSelect2").value;
 
-    const flavors = {
-        "A": "Apple Pie",
-        "B": "Banana Split",
-        "C": "Chocolate Fudge",
-        "D": "Dark Chocolate",
-        "E": "Espresso Crunch",
-        "F": "French Vanilla",
-        "G": "Ginger Snap",
-        "H": "Honey Lavender",
-        "I": "Irish Cream",
-        "J": "Jelly",
-        "K": "Kiwi",
-        "L": "Lemon",
-        "M": "Mint Chocolate Chip",
-        "N": "Nutella Swirl",
-        "O": "Orange Sherbet",
-        "P": "Peanut Butter Cup",
-        "Q": "Quince Almond",
-        "R": "Raspberry Ripple",
-        "S": "Strawberry Shortcake",
-        "T": "Toffee Crunch",
-        "U": "Ube Delight",
-        "V": "Vanilla Bean",
-        "W": "Walnut Caramel",
-        "X": "Xtra Strawberry Shortcake",
-        "Y": "Yogurt Berry",
-        "Z": "Zesty Lemon Lime"
+
+    let flavorCombinations = {
+        "cheesecake-chocolate": ["Chocolate Cheesecake Swirl", "images/chocolate_vanilla.jpg", "/core-2-interaction-project-2/assets/chocolate-cheesecake.png"],
+        "chocolate-sorbet": ["Chocolate Shortcake Swirl", "images/strawberry_mint.jpg", "/core-2-interaction-project-2/assets/chocolate-sorbet.png"],
+        "chip-chocolate": ["EXTRA Chocolate Chocolate Chip Swirl", "images/caramel_chocolate.jpg", "/core-2-interaction-project-2/assets/chocolate-chip.png"],
+        "chocolate-white": ["CHOCOLATE BOMB!", "images/caramel_chocolate.jpg", "/core-2-interaction-project-2/assets/chocolate-white.png"],
+        "chocolate-hazelnut": ["Chocolate Hazelnut Swirl", "images/strawberry_caramel.jpg", "/core-2-interaction-project-2/assets/chocolate-hazel.png"],
+        "cheesecake-vanilla": ["Vanilla Cheesecake Swirl", "images/chocolate_vanilla.jpg", "/core-2-interaction-project-2/assets/vanilla-cheesecake.png"],
+        "sorbet-vanilla": ["Vanilla Shortcake Swirl", "images/strawberry_mint.jpg", "/core-2-interaction-project-2/assets/vanilla-sorbet.png"],
+        "chip-vanilla": ["Vanilla Chocolate Chip Swirl", "images/caramel_chocolate.jpg", "/core-2-interaction-project-2/assets/vanilla-chip.png"],
+        "vanilla-white": ["Sorry but this is just a White Chocolate Swirl (try harder)", "images/vanilla_mint.jpg", "/core-2-interaction-project-2/assets/vanilla-white.png"],
+        "hazelnut-vanilla": ["Vanilla Hazelnut Swirl", "images/strawberry_caramel.jpg", "/core-2-interaction-project-2/assets/vanilla-hazelnut.png"],
+        "cheesecake-strawberry": ["Strawberry Cheesecake Swirl", "images/chocolate_vanilla.jpg", "/core-2-interaction-project-2/assets/strawberry-cheescake.png"],
+        "sorbet-strawberry": ["FRESH STRAWBERRY SORBET (the best)", "images/chocolate_vanilla.jpg", "/core-2-interaction-project-2/assets/strawberry-sorbet.png"],
+        "chip-strawberry": ["Strawberry Chocolate Chip Swirl", "images/caramel_chocolate.jpg", "/core-2-interaction-project-2/assets/strawberry-chip.png"],
+        "strawberry-white": ["White Chocolate Strawberry Swirl", "images/vanilla_mint.jpg", "/core-2-interaction-project-2/assets/strawberyy-white.png"],
+        "hazelnut-strawberry": ["No sorry this is a crime", "images/strawberry_caramel.jpg", "images/bg_strawberry_caramel.jpg"],
+        "cheesecake-mint": ["Mint Cheesecake Swirl (pls go with another one) ","images/strawberry_caramel.jpg", "/core-2-interaction-project-2/assets/mint-cheesecake.png"],
+        "mint-sorbet": ["Mint Shortcake Swirl", "images/strawberry_mint.jpg", "images/bg_strawberry_mint.jpg"],
+        "chip-mint": ["Mint Chocolate Chip Swirl", "images/caramel_chocolate.jpg", "/core-2-interaction-project-2/assets/mint-chip.png"],
+        "white-mint": ["Mint Cookie Dough Swirl", "images/vanilla_mint.jpg", "images/bg_vanilla_mint.jpg"],
+        "hazelnut-mint": ["No sorry this is a crime", "images/strawberry_caramel.jpg", "images/bg_strawberry_caramel.jpg"],
+        "caramel-cheesecake": ["Caramel Cheesecake Swirl", "images/chocolate_vanilla.jpg", "images/bg_chocolate_vanilla.jpg"],
+        "caramel-sorbet": ["Caramel Shortcake Swirl", "images/strawberry_mint.jpg", "images/bg_strawberry_mint.jpg"],
+        "caramel-chip": ["Caramel Chocolate Chip Swirl", "images/caramel_chocolate.jpg", "images/bg_caramel_chocolate.jpg"],
+        "caramel-white": ["Caramel Cookie Dough Swirl", "images/vanilla_mint.jpg", "images/bg_vanilla_mint.jpg"],
+        "caramel-hazelnut": ["Caramel Hazelnut Swirl", "images/strawberry_caramel.jpg", "images/bg_strawberry_caramel.jpg"]
     };
+    
+    const comboKey = [flavor1, flavor2].sort().join("-");
+    
 
-    // Default values if combination is not found
-    let flavor1 = flavors[input1_letter] || "Mystery";
-    let flavor2 = flavors[input2_letter] || "Surprise";
-    let finalFlavor = `${flavor1} & ${flavor2}`;
+    let finalFlavor = "Mystery Flavor! Try again!";
+    let bgSrc = "";
 
-    // Placeholder image
-    let imgSrc = "https://via.placeholder.com/200?text=Delicious+Ice+Cream";
+    if (flavorCombinations[comboKey]) {
+        finalFlavor = flavorCombinations[comboKey][0];
+        bgSrc = flavorCombinations[comboKey][2];
+    }
 
-    // Display results
+
     flavorOutput.innerText = finalFlavor;
-    imgElement.src = imgSrc;
-    imgElement.style.display = "block";
+    
+
+    let bgElement = document.createElement("div");
+    bgElement.style.position = "fixed";
+    bgElement.style.top = "0";
+    bgElement.style.left = "0";
+    bgElement.style.width = "100%";
+    bgElement.style.height = "100%";
+    bgElement.style.backgroundImage = `url('${bgSrc}')`;
+    bgElement.style.backgroundSize = "cover";
+    bgElement.style.backgroundPosition = "center center";
+    bgElement.style.opacity = "0";
+    bgElement.style.zIndex = "-1";
+    bgElement.style.animation = "fadeInBackground 1s ease-in-out forwards";
+    
+    document.body.appendChild(bgElement);
 }
